172407d287bfc0e9f8e9600b0e06c6a8c374df94
alex <alexe_webmail@yahoo.com>
2017-11-02 10:15:25 -0400

    Fix Bug R902-44: Allow state class names to be something other than "state label x: state label y"

--- DataSheets/StateClassDataSheet.vb
+++ DataSheets/StateClassDataSheet.vb
@@ -6,8 +6,8 @@
 '*********************************************************************************************
 
 Imports SyncroSim.Core
-Imports System.Globalization
 Imports System.Reflection
+Imports System.Globalization
 
 <ObfuscationAttribute(Exclude:=True, ApplyToMembers:=False)>
 Class StateClassDataSheet
@@ -15,104 +15,101 @@ Class StateClassDataSheet
 
     Private m_SlxSheet As DataSheet
     Private m_SlySheet As DataSheet
-    Private m_IsDisposed As Boolean
-
-    Protected Overrides Sub Dispose(disposing As Boolean)
-
-        If (disposing And Not Me.m_IsDisposed) Then
-
-            Me.RemoveHandlers()
-            Me.m_IsDisposed = True
-
-        End If
-
-        MyBase.Dispose(disposing)
-
-    End Sub
 
     Protected Overrides Sub OnDataFeedsRefreshed()
 
         MyBase.OnDataFeedsRefreshed()
 
-        Me.RemoveHandlers()
-
         Me.m_SlxSheet = Me.Project.GetDataSheet(DATASHEET_STATE_LABEL_X_NAME)
         Me.m_SlySheet = Me.Project.GetDataSheet(DATASHEET_STATE_LABEL_Y_NAME)
 
-        Me.AddHandlers()
-
     End Sub
 
-    Private Sub AddHandlers()
+    Public Overrides Sub Validate(proposedRow As DataRow, transferMethod As DataTransferMethod)
 
-        If (Me.m_SlxSheet IsNot Nothing) Then
-            AddHandler Me.m_SlxSheet.RowsModified, AddressOf Me.OnSlxSlyRowsModified
+        Dim ResetNull As Boolean = False
+
+        'It's not possible to have NULL for a display member, but we want the user to be able to
+        'have this name be auto-generated.  To avoid a validation failure because of a NULL here
+        'we are going to fix up the Name with a default value until the validation is done and then
+        'set it back to NULL.  The OnRowsAdded() override will auto-generate the name.  Note that
+        'this should only be done for detached rows (i.e. rows that are being editing by a control
+        'such as the DataGridView...)
+
+        If (proposedRow(DATASHEET_NAME_COLUMN_NAME) Is DBNull.Value And
+            proposedRow.RowState = DataRowState.Detached) Then
+
+            proposedRow(DATASHEET_NAME_COLUMN_NAME) = "__SYNCROSIM_TEMP_VALUE__"
+            ResetNull = True
+
         End If
 
-        If (Me.m_SlySheet IsNot Nothing) Then
-            AddHandler Me.m_SlySheet.RowsModified, AddressOf Me.OnSlxSlyRowsModified
-        End If
+        Try
+            MyBase.Validate(proposedRow, transferMethod)
+        Finally
 
-    End Sub
+            If (ResetNull) Then
+                proposedRow(DATASHEET_NAME_COLUMN_NAME) = DBNull.Value
+            End If
 
-    Private Sub RemoveHandlers()
+        End Try
 
-        If (Me.m_SlxSheet IsNot Nothing) Then
-            RemoveHandler Me.m_SlxSheet.RowsModified, AddressOf Me.OnSlxSlyRowsModified
-        End If
-
-        If (Me.m_SlySheet IsNot Nothing) Then
-            RemoveHandler Me.m_SlySheet.RowsModified, AddressOf Me.OnSlxSlyRowsModified
-        End If
-
-    End Sub
-
-    Private Sub OnSlxSlyRowsModified(ByVal sender As Object, ByVal e As DataSheetRowEventArgs)
-        Me.UpdateNames()
     End Sub
 
     Protected Overrides Sub OnRowsAdded(sender As Object, e As DataSheetRowEventArgs)
 
         MyBase.OnRowsAdded(sender, e)
-        Me.UpdateNames()
 
-    End Sub
+        Dim d As New Dictionary(Of String, Boolean)
 
-    Protected Overrides Sub OnRowsModified(sender As Object, e As DataSheetRowEventArgs)
+        'Create a dictionary of the existing names
 
-        MyBase.OnRowsModified(sender, e)
-        Me.UpdateNames()
+        For Each dr As DataRow In Me.GetData().Rows
 
-    End Sub
+            If (dr.RowState = DataRowState.Deleted) Then
+                Continue For
+            End If
 
-    Protected Overrides Sub OnRowsDeleted(sender As Object, e As DataSheetRowEventArgs)
+            If (dr(DATASHEET_NAME_COLUMN_NAME) IsNot DBNull.Value) Then
+                d.Add(CStr(dr(DATASHEET_NAME_COLUMN_NAME)), True)
+            End If
 
-        MyBase.OnRowsDeleted(sender, e)
-        Me.UpdateNames()
+        Next
 
-    End Sub
+        'If any name is NULL then add the name, checking for duplicates as we go
 
-    Public Overrides Sub Validate(ByVal proposedRow As DataRow, transferMethod As DataTransferMethod)
+        For Each dr As DataRow In Me.GetData().Rows
 
-        If (proposedRow(DATASHEET_STATECLASS_STATE_LABEL_X_ID_COLUMN_NAME) Is DBNull.Value Or
-            proposedRow(DATASHEET_STATECLASS_STATE_LABEL_Y_ID_COLUMN_NAME) Is DBNull.Value) Then
+            If (dr.RowState = DataRowState.Deleted) Then
+                Continue For
+            End If
 
-            Dim msg As String = String.Format(CultureInfo.InvariantCulture, "You must choose both a '{0}' and a '{1}'.",
-                Me.Columns(DATASHEET_STATECLASS_STATE_LABEL_X_ID_COLUMN_NAME).DisplayName,
-                Me.Columns(DATASHEET_STATECLASS_STATE_LABEL_Y_ID_COLUMN_NAME).DisplayName)
+            If (dr(DATASHEET_NAME_COLUMN_NAME) Is DBNull.Value) Then
 
-            Throw New DataException(msg)
+                Dim slxdata As DataTable = Me.m_SlxSheet.GetData()
+                Dim slydata As DataTable = Me.m_SlySheet.GetData()
+                Dim slxid As Integer = CInt(dr(DATASHEET_STATECLASS_STATE_LABEL_X_ID_COLUMN_NAME))
+                Dim slyid As Integer = CInt(dr(DATASHEET_STATECLASS_STATE_LABEL_Y_ID_COLUMN_NAME))
+                Dim slxname As String = CStr(DataTableUtilities.GetTableValue(slxdata, Me.m_SlxSheet.ValueMember, slxid, DATASHEET_NAME_COLUMN_NAME))
+                Dim slyname As String = CStr(DataTableUtilities.GetTableValue(slydata, Me.m_SlySheet.ValueMember, slyid, DATASHEET_NAME_COLUMN_NAME))
 
-        End If
+                Dim n As String = GetNextName(slxname & ":" & slyname, d)
+                dr(DATASHEET_NAME_COLUMN_NAME) = n
 
-        Me.UpdateName(proposedRow)
-        MyBase.Validate(proposedRow, transferMethod)
+                d.Add(n, True)
+
+            End If
+
+        Next
 
     End Sub
 
     Protected Overrides Sub BeforeImportData(ByVal proposedData As DataTable)
 
         MyBase.BeforeImportData(proposedData)
+
+        'We require the State Label X and Y values but they are not validated 
+        'until after the call to BeforeImportData() so validate them now.
 
         For Each dr As DataRow In proposedData.Rows
 
@@ -130,48 +127,62 @@ Class StateClassDataSheet
 
             End If
 
-            dr(DATASHEET_NAME_COLUMN_NAME) =
-                CStr(dr(DATASHEET_STATECLASS_STATE_LABEL_X_ID_COLUMN_NAME)) &
-                ":" &
-                CStr(dr(DATASHEET_STATECLASS_STATE_LABEL_Y_ID_COLUMN_NAME))
+        Next
+
+        'Create a dictionary of the existing names
+
+        Dim d As New Dictionary(Of String, Boolean)
+
+        For Each dr As DataRow In proposedData.Rows
+
+            If (dr(DATASHEET_NAME_COLUMN_NAME) IsNot DBNull.Value) Then
+                d.Add(CStr(dr(DATASHEET_NAME_COLUMN_NAME)), True)
+            End If
+
+        Next
+
+        'If any name is NULL then add the name, checking for duplicates as we go
+
+        For Each dr As DataRow In proposedData.Rows
+
+            If (dr(DATASHEET_NAME_COLUMN_NAME) Is DBNull.Value) Then
+
+                Dim InitialName As String =
+                    CStr(dr(DATASHEET_STATECLASS_STATE_LABEL_X_ID_COLUMN_NAME)) &
+                    ":" &
+                    CStr(dr(DATASHEET_STATECLASS_STATE_LABEL_Y_ID_COLUMN_NAME))
+
+                Dim FinalName As String = GetNextName(InitialName, d)
+                dr(DATASHEET_NAME_COLUMN_NAME) = FinalName
+
+                d.Add(FinalName, True)
+
+            End If
 
         Next
 
     End Sub
 
-    Private Sub UpdateNames()
+    Private Shared Function GetNextName(
+        ByVal proposedName As String,
+        ByVal existingNames As Dictionary(Of String, Boolean)) As String
 
-        Dim dt As DataTable = Me.GetData()
+        Debug.Assert(Not String.IsNullOrEmpty(proposedName))
+        Dim NewProposedName As String = proposedName
 
-        If (dt.Rows.Count > 0) Then
+        Dim NextNum As Integer = 1
+        Dim NextName As String = NewProposedName
 
-            For Each dr As DataRow In dt.Rows
+        While (existingNames.ContainsKey(NextName))
 
-                If (dr.RowState <> DataRowState.Deleted) Then
-                    Me.UpdateName(dr)
-                End If
+            NextNum += 1
+            NextName = String.Format(CultureInfo.InvariantCulture, "{0} ({1})", NewProposedName, NextNum)
+            Debug.Assert(NextNum < 500)
 
-            Next
+        End While
 
-            Me.Changes.Add(New ChangeRecord(Me, "Name Changed"))
+        Return NextName
 
-        End If
-
-    End Sub
-
-    Private Sub UpdateName(ByVal dr As DataRow)
-
-        Dim slxid As Integer = CInt(dr(DATASHEET_STATECLASS_STATE_LABEL_X_ID_COLUMN_NAME))
-        Dim slyid As Integer = CInt(dr(DATASHEET_STATECLASS_STATE_LABEL_Y_ID_COLUMN_NAME))
-
-        Dim slx As String = CStr(DataTableUtilities.GetTableValue(
-                Me.m_SlxSheet.GetData(), Me.m_SlxSheet.ValueMember, slxid, DATASHEET_NAME_COLUMN_NAME))
-
-        Dim sly As String = CStr(DataTableUtilities.GetTableValue(
-                Me.m_SlySheet.GetData(), Me.m_SlySheet.ValueMember, slyid, DATASHEET_NAME_COLUMN_NAME))
-
-        dr(DATASHEET_NAME_COLUMN_NAME) = slx & ":" & sly
-
-    End Sub
+    End Function
 
 End Class

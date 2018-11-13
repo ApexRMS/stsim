08f621ae90609a87202a15ada460328f9020ad2d
alex <alexe_webmail@yahoo.com>
2018-11-12 16:00:02 -0800

    Update version numbers for 2.0.36 release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-[assembly: AssemblyVersion("3.1.35.0")]
-[assembly: AssemblyFileVersion("3.1.35.0")]
+[assembly: AssemblyVersion("3.1.36.0")]
+[assembly: AssemblyFileVersion("3.1.36.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 

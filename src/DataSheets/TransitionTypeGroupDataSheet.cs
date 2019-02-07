e6b0782f1badaa1d0f5574e5f48e0a9448011603
alex <alexe_webmail@yahoo.com>
2019-02-06 17:16:56 -0800

    Prevent command line deletion of AUTO groups and associated linkages.

---
+++ src/DataSheets/TransitionTypeGroupDataSheet.cs
@@ -0,0 +1,30 @@
+﻿// stsim: A SyncroSim Package for developing state-and-transition simulation models using ST-Sim.
+// Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+
+using System.Data;
+using SyncroSim.Core;
+using System.Collections.Generic;
+
+namespace SyncroSim.STSim
+{
+    class TransitionTypeGroupDataSheet : DataSheet
+    {
+        public override void DeleteRows(IEnumerable<DataRow> rows)
+        {
+            List<DataRow> l = new List<DataRow>();
+
+            foreach (DataRow dr in rows)
+            {
+                if (!DataTableUtilities.GetDataBool(dr, Strings.IS_AUTO_COLUMN_NAME))
+                {
+                    l.Add(dr);
+                }
+            }
+
+            if (l.Count > 0)
+            {
+                base.DeleteRows(l);
+            }
+        }
+    }
+}

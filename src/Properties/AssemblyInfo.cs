0daa02052c5c9e1d43c145f8f703dd170823c2eb
alex <alexe_webmail@yahoo.com>
2020-11-04 10:41:47 -0800

    Update version numbers and templates for v3.2.24 release.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-[assembly: AssemblyVersion("3.2.23.0")]
-[assembly: AssemblyFileVersion("3.2.23.0")]
+[assembly: AssemblyVersion("3.2.24.0")]
+[assembly: AssemblyFileVersion("3.2.24.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 

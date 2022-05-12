be948c4518344d8e9dbc3a803e7a3cce895d3455
alexembrey <alex.embrey@gmail.com>
2022-05-12 13:14:48 -0700

    Update version to 3.3.8

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-[assembly: AssemblyVersion("3.3.7.0")]
-[assembly: AssemblyFileVersion("3.3.7.0")]
+[assembly: AssemblyVersion("3.3.8.0")]
+[assembly: AssemblyFileVersion("3.3.8.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 

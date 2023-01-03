604727a98bdf295b0fa116da191dfa5467a7cd8b
Katie Birchard <33553358+katieb1@users.noreply.github.com>
2023-01-03 11:14:01 -0800

    Update copyright headers for 2023

--- src/Runtime/InitialConditionsSpatialCollection.cs
+++ src/Runtime/InitialConditionsSpatialCollection.cs
@@ -1,5 +1,5 @@
 ﻿// stsim: A SyncroSim Package for developing state-and-transition simulation models using ST-Sim.
-// Copyright © 2007-2021 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+// Copyright © 2007-2023 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;

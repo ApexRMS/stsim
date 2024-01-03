b6e09f309da6398d48378ec1191d1431a66ba1c3
katieb1 <birchardkatie@gmail.com>
2024-01-03 09:37:44 -0800

    Update copyright to 2024

--- src/Runtime/TransitionStratumStateClass.cs
+++ src/Runtime/TransitionStratumStateClass.cs
@@ -1,5 +1,5 @@
 ﻿// stsim: A SyncroSim Package for developing state-and-transition simulation models using ST-Sim.
-// Copyright © 2007-2023 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+// Copyright © 2007-2024 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
 
 namespace SyncroSim.STSim
 {

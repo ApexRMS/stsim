c4eb3feea7f52c9a3dc1d31273891c84738e5306
Christopher Givelas <christopher.givelas@apexrms.com>
2024-04-12 14:28:31 -0400

    Copy initial condition classes from stsimresolution

---
+++ src/RuntimeRes/InitialConditionsFineSpatial.cs
@@ -0,0 +1,75 @@
+﻿// stsim: A SyncroSim Package for developing state-and-transition simulation models using ST-Sim.
+// Copyright © 2007-2024 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+
+namespace SyncroSim.STSim
+{
+    internal class InitialConditionsFineSpatial
+    {
+        private int? m_Iteration;
+        private string m_PrimaryStratumFileName;
+        private string m_SecondaryStratumFileName;
+        private string m_TertiaryStratumFileName;
+        private string m_StateClassFileName;
+        private string m_AgeFileName;
+
+        public InitialConditionsFineSpatial(
+            int? iteration, string primaryStratumName, string secondaryStratumFileName,
+            string tertiaryStratumFileName, string stateClassFileName, string ageFileName)
+        {
+            this.m_Iteration = iteration;
+            this.m_PrimaryStratumFileName = primaryStratumName;
+            this.m_SecondaryStratumFileName = secondaryStratumFileName;
+            this.m_TertiaryStratumFileName = tertiaryStratumFileName;
+            this.m_StateClassFileName = stateClassFileName;
+            this.m_AgeFileName = ageFileName;
+        }
+
+        public int? Iteration
+        {
+            get
+            {
+                return this.m_Iteration;
+            }
+        }
+
+        public string PrimaryStratumFileName
+        {
+            get
+            {
+                return this.m_PrimaryStratumFileName;
+            }
+        }
+
+        public string SecondaryStratumFileName
+        {
+            get
+            {
+                return this.m_SecondaryStratumFileName;
+            }
+        }
+
+        public string TertiaryStratumFileName
+        {
+            get
+            {
+                return this.m_TertiaryStratumFileName;
+            }
+        }
+
+        public string StateClassFileName
+        {
+            get
+            {
+                return this.m_StateClassFileName;
+            }
+        }
+
+        public string AgeFileName
+        {
+            get
+            {
+                return this.m_AgeFileName;
+            }
+        }
+    }
+}
---
+++ src/RuntimeRes/InitialConditionsFineSpatialMap.cs
@@ -0,0 +1,48 @@
+﻿// stsimresolution: SyncroSim Add-On Package (to stsim) that enables multiple raster resolutions for spatial simulations.
+// Copyright © 2007-2021 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+
+using SyncroSim.Apex;
+using System.Collections.Generic;
+
+namespace SyncroSim.STSim
+{
+    internal class InitialConditionsFineSpatialMap
+    {
+        private List<InitialConditionsSpatial> m_AllItems = new List<InitialConditionsSpatial>();
+        private SortedKeyMap1<InitialConditionsSpatial> m_Map = new SortedKeyMap1<InitialConditionsSpatial>(SearchMode.ExactPrev);
+
+        public List<InitialConditionsSpatial> AllItems
+        {
+            get
+            {
+                return this.m_AllItems;
+            }
+        }
+
+        public InitialConditionsFineSpatialMap(InitialConditionsSpatialCollection ics)
+        {
+            foreach (InitialConditionsSpatial t in ics)
+            {
+                this.AddICS(t);
+            }
+        }
+
+        private void AddICS(InitialConditionsSpatial ics)
+        {
+            this.m_Map.AddItem(ics.Iteration, ics);
+            this.m_AllItems.Add(ics);
+        }
+
+        public InitialConditionsSpatial GetICS(int? iteration)
+        {
+            if (this.m_AllItems.Count == 0)
+            {
+                return null;
+            }
+
+            InitialConditionsSpatial l = this.m_Map.GetItem(iteration);
+            return l;
+        }
+    }
+
+}

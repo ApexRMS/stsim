3655328f08607ad792bf4e3e96ad828935938ac3
Christopher Givelas <christopher.givelas@apexrms.com>
2024-07-11 14:16:56 -0400

    Add TransitionSpatialInitiationMultiplier classes to RuntimeRes

---
+++ src/RuntimeRes/TransitionSpatialInitiationMultiplierCollectionFineRes.cs
@@ -0,0 +1,15 @@
+﻿// stsim: A SyncroSim Package for developing state-and-transition simulation models using ST-Sim.
+// Copyright © 2007-2024 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+
+using System.Collections.ObjectModel;
+
+namespace SyncroSim.STSim
+{
+    internal class TransitionSpatialInitiationMultiplierCollectionFineRes : KeyedCollection<int, TransitionSpatialInitiationMultiplierFineRes>
+    {
+        protected override int GetKeyForItem(TransitionSpatialInitiationMultiplierFineRes item)
+        {
+            return item.TransitionSpatialInitiationMultiplierId;
+        }
+    }
+}
